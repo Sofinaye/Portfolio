75ef451b714a874b7f53265b9a9239d96f45adc4
Sofinaye <abene.fekadu@gmail.com>
2023-02-07 06:28:56 +0300

    Add javascript

---
+++ index.js
@@ -0,0 +1,11 @@
+const menuIcon = document.querySelector('#menu-icon')
+const menuToggle = document.querySelector('#menu-toggle')
+const menuSection = document.querySelector('#menu-section')
+
+menuIcon.addEventListener('click', ()=>{
+  menuSection.classList.add('menu-section-active')
+} )
+
+menuToggle.addEventListener('click', ()=>{
+  menuSection.classList.remove('menu-section-active')
+} )
