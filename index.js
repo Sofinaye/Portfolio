9e2f4ae2b528a3f97e81bebd08eb9820c311987f
Sofinaye <abene.fekadu@gmail.com>
2023-02-09 12:51:04 +0300

    Add project details into javascript object

--- index.js
+++ index.js
@@ -18,3 +18,41 @@ document.querySelectorAll('.menu-item a').forEach((item)=> {
     document.body.style.overflowY = 'scroll';
   })
 })
+
+const projectWindow = {
+  0: {
+    title: "Multi-Post Stories",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. This has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standar dummy text.",
+    image: "./img/tonic2.png"
+  },
+  1: {
+    title: "Professional art printing data",
+    description: "A daily selection of privately personalized reads; no accounts of sign-ups required. has been the industry's standard",
+    image: "./img/art1.png"
+  },
+  2: {
+    title: "Professional art printing data",
+    description: "A daily selection of privately personalized reads; no accounts of sign-ups required. has been the industry's standard",
+    image: "./img/art1.png"
+  },
+  3: {
+    title: "Professional art printing data",
+    description: "A daily selection of privately personalized reads; no accounts of sign-ups required. has been the industry's standard",
+    image: "./img/art1.png"
+  },
+  4: {
+    title: "Professional art printing data",
+    description: "A daily selection of privately personalized reads; no accounts of sign-ups required. has been the industry's standard",
+    image: "./img/art1.png"
+  },
+  5: {
+    title: "Professional art printing data",
+    description: "A daily selection of privately personalized reads; no accounts of sign-ups required. has been the industry's standard",
+    image: "./img/art1.png"
+  },
+  6: {
+    title: "Professional art printing data",
+    description: "A daily selection of privately personalized reads; no accounts of sign-ups required. has been the industry's standard",
+    image: "./img/art1.png"
+  }
+}
