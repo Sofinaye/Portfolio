9ea3be067338b6a3d9d8c52a1a81241324e22e6e
Sofinaye <abene.fekadu@gmail.com>
2023-02-11 21:08:12 +0300

    fix some class issues

--- store.js
+++ store.js
@@ -1,17 +1,17 @@
 const data = {};
 document.addEventListener('keyup', () => {
-  data.name = document.querySelector('.fname-input-fld').value;
-  data.email = document.querySelector('.email-input-fld').value;
-  data.text = document.querySelector('.text-input-fld').value;
+  data.name = document.querySelector('.fullName').value;
+  data.email = document.querySelector('.email').value;
+  data.text = document.querySelector('.textArea').value;
   const dataCollected = JSON.stringify(data);
   localStorage.setItem('dataTotal', dataCollected);
 });
 
 const storeItem = () => {
   const parsedData = JSON.parse(localStorage.getItem('data'));
-  const name = document.querySelector('.fname-input-fld');
-  const email = document.querySelector('.email-input-fld');
-  const text = document.querySelector('.text-input-fld');
+  const name = document.querySelector('.fullName');
+  const email = document.querySelector('.email');
+  const text = document.querySelector('.textArea');
   name.value = parsedData.name;
   email.value = parsedData.email;
   text.value = parsedData.text;
